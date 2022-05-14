8f6113ad0d63d2b31cfacf4c1010d64aa50f53e6
abdirahmanabdullahi <33281290+abdirahmanabdullahi@users.noreply.github.com>
2022-05-14 20:06:34 +0300

    Add files via upload

---
+++ fizzBuzz js/fizzBuzz.js
@@ -0,0 +1,24 @@
+function fizzBuzz() {
+    var i = 0
+    while (i < 100) {
+        i++
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log("fizzBuzz")
+        } else {
+            if (i % 3 == 0) {
+                console.log("fizz")
+            } else {
+                if (i % 5 == 0) {
+                    console.log("buzz")
+                } else {
+                    console.log(i)
+                }
+
+            }
+        }
+    }
+}
+fizzBuzz()
+
+
